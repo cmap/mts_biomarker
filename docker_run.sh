2652f86586f92a93ec5268ef2c3839e2756c3cd7
Jacob Asiedu <jasiedu@broadinstitute.org>
2020-07-30 15:57:25 -0400

    Adding  dockerignore

--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
 # -i = full path to data directory (single project or compound)
 # -o = full path to output directory
 
-docker run -it -v /Users/aboghoss/Downloads/PDEV:/data \
-  -v /Users/aboghoss/.taiga/token:/root/.taiga/token \
-  cmap/mts_biomarker -i /data/AZ-628 -o /data/results/AZ-628
+docker run -it -v /Users/jasiedu/Desktop/PDEV_PR300PLUS/RITA:/data \
+  -v /Users/jasiedu/.taiga:/root/.taiga \
+  -v /Users/jasiedu/WebstormProjects/mts_biomarker/results/RITA2:/out \
+  cmap/mts_biomarker -i /data -o /out
